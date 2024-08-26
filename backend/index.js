5ecb3cdb7118d4be00969c8e60687236e8c47d2d
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2024-08-26 02:52:36 +0200

    Book Store

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3001"],
+    origin: ["https://bookstore-ui-zeta.vercel.app"],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
   })
